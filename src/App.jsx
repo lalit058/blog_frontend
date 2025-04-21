d752d93d26bfcfdbc929c668f4e08bec6b3ce8e9
lalit058 <lalitnegi2058@gmail.com>
2025-04-21 17:02:05 +0545

    commit init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './Context/AuthContext.jsx';
 import AuthForm from './Components/AuthForm.jsx';
 import Home from './Components/Home.jsx';
 import ProtectedRoute from './Components/ProtectedRoute.jsx';
